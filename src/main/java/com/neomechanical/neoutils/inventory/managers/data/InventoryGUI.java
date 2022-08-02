f0f5d773faa65f34cd30214b7a6faf193a9309ab
KyTDK <71435411+KyTDK@users.noreply.github.com>
2022-08-02 17:56:05 +1000

    Auto pag

--- src/main/java/com/neomechanical/neoutils/inventory/managers/data/InventoryGUI.java
+++ src/main/java/com/neomechanical/neoutils/inventory/managers/data/InventoryGUI.java
@@ -1,7 +1,11 @@
 package com.neomechanical.neoutils.inventory.managers.data;
 
+import com.neomechanical.neoutils.inventory.InventoryUtil;
 import com.neomechanical.neoutils.inventory.NInventory;
+import com.neomechanical.neoutils.items.ItemUtil;
 import lombok.Data;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -14,6 +18,7 @@ import java.util.List;
 public class InventoryGUI implements NInventory {
     private @NotNull final Inventory inventory;
     private final List<InventoryItem> inventoryItems = new ArrayList<>();
+    private @NotNull final String title;
 
     @Override
     public int getSize() {
@@ -35,6 +40,18 @@ public class InventoryGUI implements NInventory {
     @Override
     public void addItem(@NotNull InventoryItem... items) throws IllegalArgumentException {
         for (InventoryItem item : items) {
+            if (getContents().length+1 > getSize()) {
+                inventory.setItem(48, ItemUtil.createItem(Material.DARK_OAK_BUTTON, ChatColor.GREEN + "Left"));
+                inventory.setItem(50, ItemUtil.createItem(Material.DARK_OAK_BUTTON, ChatColor.GREEN + "Right"));
+                List<InventoryItem> carryOver = new ArrayList<>();
+                for (int i = 0; i < getSize()-8; i++) {
+                    carryOver.add(getItem(i));
+                }
+                InventoryGUI newPage = InventoryUtil.createInventoryGUI(null, getSize(), title);
+                for (int i = 0; i < carryOver.size(); i++) {
+                    newPage.setItem(i, carryOver.get(i));
+                }
+            }
             inventoryItems.add(item);
             inventory.addItem(item.getItem());
         }
