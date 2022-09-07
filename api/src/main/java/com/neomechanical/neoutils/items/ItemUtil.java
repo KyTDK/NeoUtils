970f1f8ba854e6f10259bb1a7f05f7e735f949c7
KyTDK <71435411+KyTDK@users.noreply.github.com>
2022-09-07 16:40:05 +1000

    removed itemutil durabilities

--- api/src/main/java/com/neomechanical/neoutils/items/ItemUtil.java
+++ api/src/main/java/com/neomechanical/neoutils/items/ItemUtil.java
@@ -1,14 +1,11 @@
 package com.neomechanical.neoutils.items;
 
-
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class ItemUtil {
     /**
@@ -44,54 +41,6 @@ public class ItemUtil {
         item.setItemMeta(meta);
         return item;
     }
-    /**
-     * Set ItemStack's durability
-     *
-     * @param itemStack the item in which durability will be altered
-     * @param damage by how much will the durability change
-     *
-     */
-    public static void setDurability(ItemStack itemStack, int damage) {
-        org.bukkit.inventory.meta.Damageable damageableMeta =
-                (org.bukkit.inventory.meta.Damageable) itemStack.getItemMeta();
-        assert damageableMeta != null;
-        int durability =
-                damageableMeta.getDamage(); // returns the damage the item has received **this is a positive integer**
-        damageableMeta.setDamage(durability + damage);
-        itemStack.setItemMeta(damageableMeta);
-    }
-    /**
-     * Set ItemStack's durability, taking unbreaking into account
-     *
-     * @param itemStack the item in which durability will be altered
-     * @param damage by how much will the durability change
-     *
-     */
-    public static void setRealisticDurability(ItemStack itemStack, int damage) {
-        org.bukkit.inventory.meta.Damageable damageableMeta =
-                (org.bukkit.inventory.meta.Damageable) itemStack.getItemMeta();
-        assert damageableMeta != null;
-        int durability =
-                damageableMeta.getDamage(); // returns the damage the item has received **this is a positive integer**
-        if (itemStack.getEnchantments().containsKey(Enchantment.DURABILITY)) {
-            int percentage = 100 / itemStack.getEnchantmentLevel(Enchantment.DURABILITY) + 1;
-            Random rand = new Random();
-            int n = rand.nextInt(100) + 1;
-            if (n <= percentage) {
-                damageableMeta.setDamage(durability + damage);
-                itemStack.setItemMeta(damageableMeta);
-            }
-        }
-    }
-
-    public static short getDurability(ItemStack itemStack) {
-        org.bukkit.inventory.meta.Damageable damageableMeta =
-                (org.bukkit.inventory.meta.Damageable) itemStack.getItemMeta();
-        assert damageableMeta != null;
-        return (short)
-                damageableMeta.getDamage(); // returns the damage the item has received **this is a positive integer**
-    }
-
     public static short getMaxDurability(ItemStack itemStack) {
         return itemStack.getType().getMaxDurability();
     }
